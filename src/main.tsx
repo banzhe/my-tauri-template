cbeb515a74ad468b2ee9ada1c48998d9418f02ff
banzhe <banzhe5@outlook.com>
2025-12-29 11:08:55 +0800

    update window title

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { refreshTauriTitle } from './utils/tauriTitle'
+
+refreshTauriTitle()
 
 // biome-ignore lint/style/noNonNullAssertion: root
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<App />
-	</StrictMode>
+	</StrictMode>,
 )
